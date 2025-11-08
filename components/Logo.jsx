3119f66a4717f8a6714dbccf06d4472d982d8b67
samikshyapattnaik <samikshyapattnaik14@gmail.com>
2025-11-08 12:12:24 +0530

    upadate logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react' 
 import Link from 'next/link'
-import Image from 'next/image'
-const Logo = () => {
-  return (
-    <Link href="">
-        <Image src="/logo/Margam.png" width={230} height={48} alt='logo'></Image>
-    </Link>
-  )
-}
+import Image from 'next/image' 
 
+const Logo = () => { 
+    return ( 
+    <Link href="/home">
+         <Image src="/logo/Margam.png" 
+          width={230} height={48} alt='logo'>
+         </Image> 
+     </Link> 
+     
+ ) } 
 export default Logo
